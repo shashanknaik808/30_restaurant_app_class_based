3d8f267af3f5f5e092807d5b7a32e85b2e5ebd2d
Shashank Naik <shashanknaik808@gmail.com>
2023-11-07 23:44:24 +0530

    Converting Navbar to Class-Based Component

--- frontend/src/component/navbar/Navbar.jsx
+++ frontend/src/component/navbar/Navbar.jsx
@@ -1,81 +1,24 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
-
-class App extends Component {
+class Navbar extends Component {
     render() {
         return (
-            <BrowserRouter>
-                <Navbar />
-                <Routes>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/restaurant" element={<Restaurant />} />
-                </Routes>
-            </BrowserRouter>
-        );
-    }
-}
-
-class Login extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: '',
-            password: '',
-        };
-
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleInputChange(event) {
-        this.setState({ [event.target.name]: event.target.value });
-    }
-
-    handleSubmit(event) {
-        event.preventDefault();
-        const { username, password } = this.state;
-        console.log('Form data submitted:\nUsername:', username, '\nPassword:', password);
-    }
-
-
-    render() {
-        return (
-            <div className='pri'>
-                <div className='wrapper'>
-                    <h1>Login Form</h1>
-                    <form onSubmit={this.handleSubmit}>
-                        <div className='container'>
-                            <label htmlFor="username"><b>Username</b></label>
-                            <input
-                                type='text'
-                                name='username'
-                                id='username'
-                                value={this.state.username}
-                                onChange={this.handleInputChange}
-                                required
-                            />
-                            <label htmlFor="password"><b>Password</b></label>
-                            <input
-                                type='password'
-                                name='password'
-                                id='password'
-                                value={this.state.password}
-                                onChange={this.handleInputChange}
-                                required
-                            />
-                            <input type='submit' value='Login' />
-                        </div>
-                    </form>
+            <div className='navbar-container dark-mode'>
+                <div className='logo'>Heaven's Kitchen</div>
+                <div className='buttons'>
+                    <Link to="/login">
+                        <button className='nav-button'>Login</button>
+                    </Link>
+                    <br />
+                    <Link to="/restaurant">
+                        <button className='nav-button'>Restaurant</button>
+                    </Link>
                 </div>
             </div>
         );
     }
 }
-
 
 export default Navbar;
