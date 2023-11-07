564a0bf9e732ee7b9fae0c36dda3d14d16623f5f
Shashank Naik <shashanknaik808@gmail.com>
2023-11-07 14:10:15 +0530

    Created a simple login form component using React class

--- frontend/src/component/login/Login.jsx
+++ frontend/src/component/login/Login.jsx
@@ -1,11 +1,31 @@
+import React, { Component } from 'react';
 import './Login.css';
+import { useNavigate } from 'react-router-dom';
 
-function Login(props) {
-    return (
-        <div>
-            <h1>Login Component</h1>
-        </div>
-    );
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+
+    
+
+    render() {
+        return (
+            <div className='pri'>
+                <div className='wrapper'>
+                    <h1>Login Form</h1>
+                    <form onSubmit={this.handleSubmit}>
+                        Username: <input type='text' name='username' id='username' /><br />
+                        Password: <input type='password' name='password' id='password' /><br />
+                        <input type='submit' value='Login' />
+                    </form>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Login;
