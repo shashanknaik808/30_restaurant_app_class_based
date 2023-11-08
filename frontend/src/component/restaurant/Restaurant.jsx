fe3cb4b81a38f210f85b76dfeb8bed78ee3090f6
Shashank Naik <shashanknaik808@gmail.com>
2023-11-08 13:29:03 +0530

    Fetch data from backend in componentDidMount

--- frontend/src/component/restaurant/Restaurant.jsx
+++ frontend/src/component/restaurant/Restaurant.jsx
@@ -1,11 +1,27 @@
+import React, { Component } from 'react';
 import './Restaurant.css';
 
-function Restaurant(props) {
-    return (
-        <div>
-            <h1>Restaurant Component</h1>
-        </div>
-    );
+class Restaurant extends Component {
+
+    componentDidMount() {
+        this.getData();
+    }
+
+    async getData() {
+        let backend_url = 'http://localhost:3200/data';
+        let response = await fetch(backend_url);
+
+        let responseData = await response.json();
+        console.log(responseData);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Restaurant Component</h1>
+            </div>
+        );
+    }
 }
 
 export default Restaurant;
