82d016c6f9e1daca10b1f710b8a56db108eefd2d
Shashank Naik <shashanknaik808@gmail.com>
2023-11-08 15:39:55 +0530

    Refactored the App component to use class-based components for better code organization.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,18 +7,21 @@ import './App.css';
 import Navbar from './component/navbar/Navbar';
 import Login from './component/login/Login';
 import Restaurant from './component/restaurant/Restaurant';
+import { Component } from 'react';
 
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route exact path='/login' element={<Login />} />
-        <Route exact path='/restaurant' element={<Restaurant />} />
-      </Routes>
-    </BrowserRouter>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path='/login' element={<Login />} />
+          <Route exact path='/restaurant' element={<Restaurant />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
