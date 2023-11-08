8b67897b66c768ffcc88f799215b34d9d6a70b95
Shashank Naik <shashanknaik808@gmail.com>
2023-11-08 15:20:32 +0530

    Added Card component for displaying food items with images, titles, and category icons.

--- frontend/src/component/restaurant/Restaurant.jsx
+++ frontend/src/component/restaurant/Restaurant.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Restaurant.css';
+import { Card, Button } from 'react-bootstrap';
 
 class Restaurant extends Component {
     constructor(props) {
@@ -51,30 +52,49 @@ class Restaurant extends Component {
 
                 {flag ? (
                     <ul>
-                        {foodData.map((item) => (
-                            <li key={item.foodName}>
-                                Food Name is {item.foodName}, Price: {item.price}, Category: {item.category}
-                            </li>
-                        ))}
+                        <div className='food-container'>
+                            {foodData.map((item) => (
+                                <Card className='food-item' style={{ width: '18rem' }} key={item.foodName}>
+                                    <Card.Img variant="top" src={'images/food/' + item.foodName + '.jpg'} />
+                                    <Card.Body className={(item.category === 'veg') ? 'veg' : 'non-veg'}>
+                                        <Card.Title>{item.foodName} : {item.price}</Card.Title>
+                                        <Card.Text>
+                                            {item.foodName} is an amazing dish. You should try it.
+                                        </Card.Text>
+                                        <div>
+                                            Category: <img
+                                                src={(item.category === 'veg') ? 'images/veg.png' : 'images/non-veg.png'}
+                                                height="30px" width="30px"
+                                                alt={(item.category === 'veg') ? 'Veg' : 'Non-Veg'}
+                                            />
+                                        </div>
+                                        <Button variant="primary">Buy Now</Button>
+                                    </Card.Body>
+                                </Card>
+                            ))}
+                    </div>
                     </ul>
-                ) : (
-                    <h1>Loading</h1>
-                )}
+        ) : (
+            <h1>Loading</h1>
+        )
+    }
 
                 <h1> Drink Menu</h1>
 
-                {flag ? (
-                    <ul>
-                        {drinkData.map((item) => (
-                            <li key={item.drinkName}>
-                                Drink Name is {item.drinkName}, Price: {item.price}, Category: {item.category}
-                            </li>
-                        ))}
-                    </ul>
-                ) : (
-                    <h1>Loading</h1>
-                )}
-            </div>
+{
+    flag ? (
+        <ul>
+            {drinkData.map((item) => (
+                <li key={item.drinkName}>
+                    Drink Name is {item.drinkName}, Price: {item.price}, Category: {item.category}
+                </li>
+            ))}
+        </ul>
+    ) : (
+        <h1>Loading</h1>
+    )
+}
+            </div >
         );
     }
 }
