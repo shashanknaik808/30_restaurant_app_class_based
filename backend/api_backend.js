c92c4a1cd55700646840dad512fc550b03b3ecce
Shashank Naik <shashanknaik808@gmail.com>
2023-11-08 12:59:00 +0530

    Added data endpoints for food and drink information

--- backend/api_backend.js
+++ backend/api_backend.js
@@ -22,6 +22,25 @@ app.post("/login", (req, res) => {
     }
 });
 
+let foodData = [
+    { foodName: 'French Fries', price: 'Rs.250/', category: 'veg' },
+    { foodName: 'Tandoori Mushrooms', price: 'Rs.450/', category: 'veg' },
+    { foodName: 'Chicken Alabu', price: 'Rs.550/', category: 'non-veg' },
+    { foodName: 'Dragon Prawns', price: 'Rs.650/', category: 'non-veg' },
+]
+
+let drinkData = [
+    { drinkName: 'Virgin Mojito', price: 'Rs.250/', category: 'mocktail' },
+    { drinkName: 'Pinacolada', price: 'Rs.350/', category: 'mocktail' },
+    { drinkName: 'Old Fashioned', price: 'Rs.550/', category: 'cocktail' },
+    { drinkName: 'Moscow Mule', price: 'Rs.650/', category: 'cocktail' },
+]
+
+let data = [foodData, drinkData];
+app.get('/data', (req, res) => {
+    res.json(data);
+});
+
 app.listen(3200, () => {
     console.log("Backend application is running at port 3200");
 });
